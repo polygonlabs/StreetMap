6fe0d0c7cf5f2831738dcd6f1e121a41f1311148
Richard Schubert <rs@directx9.de>
2018-09-25 20:38:51 +0200

    Fixed warning during project generation

--- Source/StreetMapImporting/StreetMapImporting.Build.cs
+++ Source/StreetMapImporting/StreetMapImporting.Build.cs
@@ -37,7 +37,6 @@ namespace UnrealBuildTool.Rules
 
             PrivateIncludePaths.AddRange(
                 new string[] {
-                    "Developer/DesktopPlatform/Public",
                 }
             );
         }
